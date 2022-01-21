8b5fd51a9ca6e0d0f4db0ee39a78c99de933860b
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2022-01-21 10:48:52 +0800

    Перегрузка операторов сравнения

---
+++ U-string.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class U_string
+{
+public:
+	U_string();
+	U_string(const wchar_t* _str);
+	U_string(const wstring& _str);
+	U_string(const string& _str);
+	U_string(const char* _str);
+	U_string(const U_string& _str);
+	
+	size_t get_lenght() const;
+
+	size_t get_capacity() const;
+	
+	void clear();
+
+	void push(const wchar_t& elem);
+
+	void increase(const U_string& _data, const int position);
+	void increase(const wchar_t* _data, const int position);
+	void increase(const wstring& _data, const int position);
+	void increase(const string& _data, const int position);
+	void increase(const char* _data, const int position);
+
+	void push_back(const U_string& _data);
+	void push_back(const wchar_t* _data);
+	void push_back(const wstring& _data);
+	void push_back(const string& _data);
+	void push_back(const char* _data);
+
+	U_string cut(int _begin, int _end) const;
+	
+	vector<U_string> split(const U_string& separator, const int max_count_cut = 0) const;
+
+	vector<U_string> find(const U_string& mask_template, const int max_count_elem = -1) const;
+
+	void get_statistics() const;
+
+	U_string& operator=(const U_string& u_str);
+	U_string& operator=(const wchar_t* u_str);
+	U_string& operator=(const wstring& u_str);
+	U_string& operator=(const string& u_str);
+	U_string& operator=(const char* u_str);
+
+	bool operator==(const U_string& u_str) const;
+	bool operator!=(const U_string& u_str) const;
+
+	bool operator>(const U_string& u_str) const;
+	bool operator>=(const U_string& u_str) const;
+	bool operator<(const U_string& u_str) const;
+	bool operator<=(const U_string& u_str) const;
+
+	friend wostream& operator<<(wostream& out, const U_string& u_str);
+	friend wistream& operator>>(wistream& in, U_string& u_str);
+
+	~U_string();
+
+private:
+	wchar_t* str;
+	size_t lenght;
+	size_t capacity;
+
+	void _change_size(const size_t add);
+};
---
+++ source.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+#include <clocale>
+#include <io.h>
+#include <fcntl.h>
+
+#include "U-string.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	_setmode(_fileno(stderr), _O_U16TEXT);
+
+	wcout << L"Работа конструкторов:" << endl;
+
+	U_string example_1("example_1");
+
+	const wchar_t* c_string = L"example_2";
+	U_string example_2(c_string);
+
+	string str("example_3");
+	U_string example_3(str);
+
+	wstring wide_character_string(L"example_4");
+	U_string example_4(wide_character_string);
+
+	U_string example_5(example_1);
+
+	wcout << example_1 << " " << example_2 << " " << example_3 << " " << example_4 << " " << example_5 << " " << endl << endl;
+
+	wcout << L"Поиск по заданному разделителю:" << endl;
+	example_1 = "RUSSI";
+	example_1.push_back("A");
+	wcout << L"Поиск в строке: " << example_1 << endl;
+	wcout << L"Разделитель: " << L"S" << endl;
+	wcout << L"Результат:" << endl;
+	vector<U_string> elements;
+	elements = example_1.split( U_string("S") );
+	for (int i = 0; i < elements.size(); i++)
+	{
+		wcout << i + 1 << L" element = " << elements[i] << endl;
+	}
+
+	wcout << endl << L"Поиск в строке: " << example_1 << endl;
+	wcout << L"Разделитель: " << L"SS" << endl;
+	wcout << L"Результат:" << endl;
+	U_string separator("SS");
+	elements.clear();
+	elements = example_1.split(separator);
+	for (int i = 0; i < elements.size(); i++)
+	{
+		wcout << i + 1 << L" element = " << elements[i] << endl;
+	}
+	wcout << endl;
+
+	example_1 = "VCTTTK";
+	wcout << L"Поиск в строке подстроки, с возможностью поиска по маске (символы ? и *):" << endl;
+	separator = "*T";
+	wcout << L"Строка: " << example_1 << L";\tШаблон (маска):" << separator << endl;
+	elements.clear();
+	elements = example_1.find(separator);
+	for (int i = 0; i < elements.size(); i++)
+	{
+		wcout << i + 1 << L" element = " << elements[i] << endl;
+	}
+
+	wcout << endl;
+	example_1 = L"РИЯ";
+	wcout << L"Копирование в строку: " << example_1 << endl << L"Копируемая строка: " << L"ОСС" << endl;
+	example_1.increase(L"ОСС", 2);
+	wcout << L"Результат: " << example_1 << endl;
+
+	wcout << endl;
+	wcout << L"Копирование в конец строки: " << example_1 << endl << L"Копируемая строка: " << L"!!!" << endl;
+	const wchar_t* temp = L"!!!";
+	example_1.push_back(temp);
+	wcout << L"Результат: " << example_1 << endl;
+
+	example_1 = L"An arbitrary string for calculating word statistics and so on. Произвольная строка для подсчета статистики слов и тд.";
+	wcout << endl << L"Получение статистики:" << endl;
+	example_1.get_statistics();
+
+	wcout << endl << L"Введите строку для проверки корректности ввода: " << endl;
+	wcin >> example_1;
+	wcout << endl << L"Вы ввели: " << example_1 << endl;
+
+	return 0;
+}
